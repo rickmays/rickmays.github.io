c416509676b2d3b1c0614705de8b158c66266fd8
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-08-12 18:53:46 -0500

    add more projects and style project image display

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,25 @@ function showImage(event) {
             document.getElementById('image-1').style.visibility = 'visible';
             break;
         case 'button-2':
-            document.getElementById('image-2').style.visibility = 'visible'
+            document.getElementById('image-2').style.visibility = 'visible';
+            break;
+        case 'button-3':
+            document.getElementById('image-3').style.visibility = 'visible';
+            break;
+        case 'button-4':
+            document.getElementById('image-4').style.visibility = 'visible';
+            break;
+        case 'button-5':
+            document.getElementById('image-5').style.visibility = 'visible';
+            break;
+        case 'button-6':
+            document.getElementById('image-6').style.visibility = 'visible';
+            break;
+        case 'button-7':
+            document.getElementById('image-7').style.visibility = 'visible';
+            break;
+        case 'button-8':
+            document.getElementById('image-8').style.visibility = 'visible';
             break;
     }
 }
