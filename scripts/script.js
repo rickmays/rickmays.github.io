aa858c0afb541b34ec94dc740828418c8d1e4dc0
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-08-11 20:21:35 -0500

    flatten folder structure, get first button working

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+function showImage() {
+    document.getElementById('image-1').style.visibility = 'visible';
+}
+
+const button1 = document.getElementById('button-1');
+button1.addEventListener('click', showImage);
