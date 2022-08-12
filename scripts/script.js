4940c3f00ec9b33f5107d63ed1d669831621c0b3
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-08-12 13:22:16 -0500

    get buttons to display images (1st and 2nd button)

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,25 @@
-function showImage() {
-    document.getElementById('image-1').style.visibility = 'visible';
+function hideAllImages() {
+    let figures = document.querySelectorAll('figure');
+    for (let i of figures) {
+        i.style.visibility = 'hidden';
+      }
 }
 
-const button1 = document.getElementById('button-1');
-button1.addEventListener('click', showImage);
+function showImage(event) {
+    hideAllImages();
+    switch (event.srcElement.id) {
+        case 'button-1':
+            document.getElementById('image-1').style.visibility = 'visible';
+            break;
+        case 'button-2':
+            document.getElementById('image-2').style.visibility = 'visible'
+            break;
+    }
+}
+// add event listeners to buttons
+let buttons = document.querySelectorAll('.project-button');
+for (let i of buttons) {
+  i.addEventListener('click', showImage);
+}
+
+
